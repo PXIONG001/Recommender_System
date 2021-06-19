85e6e2a9abce95fea8da0bc05a898ce8ce4bb2aa
PXIONG001 <61022244+PXIONG001@users.noreply.github.com>
2021-06-19 15:09:16 -0500

    Peng: Practice for SVD

--- RS.py
+++ RS.py
@@ -1,4 +1,20 @@
 import numpy as np
-import pandas as pd
-import psycopg2
-import cx_Oracle
+
+from sklearn.decomposition import TruncatedSVD
+
+# Regular SVD
+A = np.array([[3,4,3],[1,2,3],[4,2,1]])
+
+U, D, VT = np.linalg.svd(A)
+
+A_remake = (U @ np.diag(D) @ VT)
+
+print(A_remake)
+
+# Truncated SVD
+B = np.array([[4,3,4],[3,2,1],[1,2,4]])
+
+trun_svd = TruncatedSVD(n_components=2)
+B_transformed = trun_svd.fit_transform(B, y=None)
+
+print(B_transformed)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+B = np.array([[4,3,4],[3,2,1],[1,2,4]])
+
+# trun_svd = TruncatedSVD(n_components=2)
+# B_transformed = trun_svd.fit_transform(B, y=None)
+
+# print(B_transformed)
