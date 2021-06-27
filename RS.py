45547cf3b1e6bead395dc6e028bc2faae21e0f2b
PXIONG001 <61022244+PXIONG001@users.noreply.github.com>
2021-06-26 21:01:33 -0500

    Peng: Fixed some major issues with errors

--- RS.py
+++ RS.py
@@ -7,14 +7,14 @@ columns = ['user_id','item_id','ratings','timestamp']
 df_read = pd.read_csv("ml-100k/u.data", sep = '\t', names=columns)
 
 
-new_columns = ['item_id', 'movie title', 'release date', 'video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
+columns = ['item_id', 'movie title', 'release date', 'video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
           'Animation', 'Childrens', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 'Horror',
           'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
 
-movies = pd.read_csv('ml-100k/u.item', sep='|',names=new_columns,encoding='latin-1')
-movie_names = [['item_ID','movie_title']]
+movies = pd.read_csv('ml-100k/u.item', sep='|', names=columns, encoding='latin-1')
+movie_names = movies[['item_id', 'movie title']]
 
 cm = pd.merge(df_read, movie_names, on='item_id')
 
-cm = cm[['user_id','movie title', 'rating']]
+cm = cm[['user_id','movie title', 'ratings']]
 cm.head()
